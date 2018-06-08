3adf6a17a5819ded1a3f851f07f6b7442d5d8ac8
Radovan Bast <bast@users.noreply.github.com>
2018-06-08 11:08:17 +0200

    add a main function

--- example.py
+++ example.py
@@ -3,3 +3,14 @@ def simple_function(x, y):
     Returns the sum of the two arguments.
     """
     return x + y
+
+
+if __name__ == '__main__':
+
+    # there are better ways to pass command line arguments
+    # to a python code but this is not the point here
+    x = float(sys.argv[-2])
+    y = float(sys.argv[-1])
+
+    result = simple_function(x, y)
+    print(result)
